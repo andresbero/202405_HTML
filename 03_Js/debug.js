d255e2a9adddb1bfa2adeeaf44348d69c0d30a2a
Andres Felipe Bermudez <andres.bermudezrojas@gmail.com>
2024-05-31 17:57:07 -0500

    Core Pagina de Perfil

---
+++ 03_Js/debug.js
@@ -0,0 +1,12 @@
+function multiplicarsumando(x,y){
+    console.log(x,y)
+    suma=0;
+    for(i=0;i<x;i++){
+        suma+=y;
+    }
+    return suma
+
+}
+
+console.log(multiplicarsumando(10,2))
+
